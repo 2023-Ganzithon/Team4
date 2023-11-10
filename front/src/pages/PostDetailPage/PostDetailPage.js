e627904a1fea0896976e486f030b82057e254b05
Youjiiin <yujinjang24@gmail.com>
2023-11-10 23:54:31 +0900

    feat: 최근 검색목록 변경 기능

--- front/src/pages/PostDetailPage/PostDetailPage.js
+++ front/src/pages/PostDetailPage/PostDetailPage.js
@@ -7,14 +7,32 @@ import BuyBtn from "./Buy/BuyBtn";
 import WriteComment from "./Comment/WriteComment";
 
 const PostDetailPage = () => {
-  const getContent = async() => {};
+  const getContent = async() => {
+    const url = '/groceries/1';
+    fetch(url, {
+      method: 'GET',
+      headers: {}
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('[ERROR] 알수없는 이유로 오류가 발생했습니다.');
+      } 
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error("[ERROR] 에러발생")
+    });
+  };
 
   return <div className={style.wrapper}>
   <div className={style.header}>
     <div className={style.back}>
       <img src={back} alt="back"/>
     </div>
-    <span>홈으로 가기</span>
+    <span onClick={getContent}>홈으로 가기</span>
   </div>
     <PostContent />
     <Comment />
--- front/src/pages/SearchPage/SearchPage.js
+++ front/src/pages/SearchPage/SearchPage.js
@@ -6,16 +6,29 @@ import cancel from "../../assets/image/cancel.png";
 const SearchPage = () => {
   const [inputText, setInputText] = useState('');
   const [recentSearchText, setRecentSearchText] = useState(["포도", "샤인머스캣", "라멘", "대파", "커피", "짜장면", "계란"]);
-  
+
   const handleInputChange = (e) => {
     setInputText(e.target.value);
   };
 
   const handleEnterPress = (e) => {
       if (e.key === 'Enter' && inputText) {
-          setRecentSearchText([...recentSearchText, inputText]);
+          keepLength(inputText);
           setInputText(''); // 입력 상자 초기화
       }
+  };
+
+  const keepLength = (newText) => {
+    setRecentSearchText(currentSearchTexts => {
+      const updatedSearchTexts = [...currentSearchTexts, newText];
+
+      // 배열 길이가 8을 초과하면, 가장 오래된 항목(첫 번째 요소)을 제거
+      if (updatedSearchTexts.length > 7) {
+          updatedSearchTexts.shift(); // 배열의 첫 번째 요소 제거
+      }
+
+      return updatedSearchTexts;
+  });
   };
 
   return <div className={style.wrapper}>
